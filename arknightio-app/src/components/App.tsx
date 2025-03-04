5d665048f7dda6cc44ff9a6c60b3dbe5f4bda252
alittlerocket <zhang144444@gmail.com>
2025-03-04 17:58:19 -0500

    Clean project.

---
+++ arknightio-app/src/components/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+// @deno-types="@types/react"
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <div>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
